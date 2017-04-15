70a3e5b5adfc863bb8c4aa22677dba1780035375
Alliston Aleixo <alliston@outlook.com>
2017-04-15 14:38:08 -0300

    Criação do projeto IAPSwift

---
+++ IAPSwift/IAPSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IAPSwift
+//
+//  Created by Alliston Aleixo on 15/04/17.
+//  Copyright © 2017 Alliston Aleixo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
